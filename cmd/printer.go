513915c0916a5ea81eac69f748030fd45069837d
nilic <github@nilic.33mail.com>
2023-07-08 10:36:14 +0200

    return separate external and HN URLs

--- cmd/printer.go
+++ cmd/printer.go
@@ -13,21 +13,26 @@ const (
 
 func (h *Hits) PrintConsole() {
 	for i, s := range h.Hits {
-		fmt.Printf("%d. %s\n%s\n%d points by %s, %s | %d comments\n\n", i+1, s.Title, s.getItemURL(), s.Points, s.Author, timeago.New(s.CreatedAt).Format(), s.NumComments)
+		fmt.Printf("%d. %s\n%s\n%s\n%d points by %s, %s | %d comments\n\n", i+1, s.Title, s.getExternalURL(), s.getItemURL(), s.Points, s.Author, timeago.New(s.CreatedAt).Format(), s.NumComments)
 	}
 }
 
 func (h *Hits) PrintHTML() {
 	for i, s := range h.Hits {
-		fmt.Printf("%d. <a href=\"%s\">%s</a>\n%d points by <a href=\"%s\">%s</a>, %s | %d comments\n\n", i+1, s.getItemURL(), s.Title, s.Points, s.getUserURL(), s.Author, timeago.New(s.CreatedAt).Format(), s.NumComments)
+		fmt.Printf("%d. <a href=\"%s\">%s</a>\n%d points by <a href=\"%s\">%s</a>, %s | <a href=\"%s\">%d comments</a>\n\n", i+1, s.getExternalURL(), s.Title, s.Points, s.getUserURL(), s.Author, timeago.New(s.CreatedAt).Format(), s.getItemURL(), s.NumComments)
 	}
 }
 
 func (h *Hit) getItemURL() string {
+	return itemBaseURL + h.ObjectID
+}
+
+func (h *Hit) getExternalURL() string {
 	if h.URL != "" {
 		return h.URL
 	}
-	return itemBaseURL + h.ObjectID
+
+	return h.getItemURL()
 }
 
 func (h *Hit) getUserURL() string {
