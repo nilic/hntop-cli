8bacf6464720f47cf02a29489f598b74ba0929ca
nilic <github@nilic.33mail.com>
2023-07-27 21:48:09 +0200

    chore: refactor

--- cmd/hntop/hntop.go
+++ cmd/hntop/hntop.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/nilic/hntop-cli/internal/client"
 	"github.com/urfave/cli/v2"
@@ -15,6 +16,24 @@ const (
 var (
 	userAgent = appName + "/" + getVersion()
 )
+
+type Hit struct {
+	CreatedAt   time.Time `json:"created_at"`
+	Title       string    `json:"title"`
+	URL         string    `json:"url"`
+	Author      string    `json:"author"`
+	Points      int       `json:"points"`
+	NumComments int       `json:"num_comments"`
+	ObjectID    string    `json:"objectID"`
+}
+
+type Hits struct {
+	Hits        []Hit `json:"hits"`
+	NbHits      int   `json:"nbHits"`
+	Page        int   `json:"page"`
+	NbPages     int   `json:"nbPages"`
+	HitsPerPage int   `json:"hitsPerPage"`
+}
 
 func Execute(cCtx *cli.Context) error {
 	q := buildQuery(cCtx)
