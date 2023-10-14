965283e366345347e7a60007617a2365020fe1a1
nilic <github@nilic.33mail.com>
2023-10-14 11:13:25 +0200

    more refactoring

--- pkg/htclient/htclient.go
+++ pkg/htclient/htclient.go
@@ -2,7 +2,6 @@ package htclient
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/nilic/hntop-cli/internal/client"
 )
@@ -14,24 +13,6 @@ const (
 type HTClient struct {
 	URL       string
 	UserAgent string
-}
-
-type Hit struct {
-	CreatedAt   time.Time `json:"created_at"`
-	Title       string    `json:"title"`
-	URL         string    `json:"url"`
-	Author      string    `json:"author"`
-	Points      int       `json:"points"`
-	NumComments int       `json:"num_comments"`
-	ObjectID    string    `json:"objectID"`
-}
-
-type Hits struct {
-	Hits        []Hit `json:"hits"`
-	NbHits      int   `json:"nbHits"`
-	Page        int   `json:"page"`
-	NbPages     int   `json:"nbPages"`
-	HitsPerPage int   `json:"hitsPerPage"`
 }
 
 func NewClient(query, userAgent string) *HTClient {
