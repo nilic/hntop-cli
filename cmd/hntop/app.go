3554c5eb57629ff7ab3807891dbcd512bb7a4361
nilic <github@nilic.33mail.com>
2023-10-16 21:21:55 +0200

    chore: refactor

--- cmd/hntop/app.go
+++ cmd/hntop/app.go
@@ -13,6 +13,20 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+const (
+	defaultResultCount = 20
+	minResultCount     = 1
+	maxResultCount     = 1000                        // maximum number of results returned by Algolia API
+	defaultTags        = "story,poll,show_hn,ask_hn" // return all post types
+)
+
+var (
+	appName          = "hntop"
+	appNameUpper     = strings.ToUpper(appName)
+	availableTags    = []string{"story", "poll", "show_hn", "ask_hn"}
+	availableOutputs = []string{"list", "mail"}
+)
+
 func newApp() *cli.App {
 	app := &cli.App{
 		Name:  appName,
--- cmd/hntop/main.go
+++ cmd/hntop/main.go
@@ -3,21 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
-)
-
-const (
-	defaultResultCount = 20
-	minResultCount     = 1
-	maxResultCount     = 1000                        // maximum number of results returned by Algolia API
-	defaultTags        = "story,poll,show_hn,ask_hn" // return all post types
-)
-
-var (
-	appName          = "hntop"
-	appNameUpper     = strings.ToUpper(appName)
-	availableTags    = []string{"story", "poll", "show_hn", "ask_hn"}
-	availableOutputs = []string{"list", "mail"}
 )
 
 func main() {
